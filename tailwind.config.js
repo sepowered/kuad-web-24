22e49169ae4bcb445c7f3cf5f684bb365f404a18
Kwonhoo Roh <sepowered@gmail.com>
2024-10-21 17:01:08 +0900

    for update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./*.{html,js,jsx,ts,tsx}",
+    "./**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
